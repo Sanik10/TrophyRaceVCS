bc90b9f270e0e1f2ade3222d305c9be0890aa27e
Sanik10 <109314889+Sanik10@users.noreply.github.com>
2024-03-11 03:14:28 +0300

    Изменение логики подвески. Изменение логики определения поверхности под колесом

--- Assets/Scripts/Suspension/Axle.cs
+++ Assets/Scripts/Suspension/Axle.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+﻿/*using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using NWH.Common.Vehicles;
@@ -8,16 +8,18 @@ using NWH.Common.Vehicles;
 /// </summary>
 public class Axle : MonoBehaviour {
 
-	[SerializeField] WheelUAPI LeftWheelCollider;					//Left WheelCollider ref
-	[SerializeField] WheelUAPI RightWheelCollider;					//Right WheelCollider ref
+	[SerializeField] Transform LeftWheelController;					//Left WheelCollider ref
+	[SerializeField] Transform RightWheelController;				//Right WheelCollider ref
 	[SerializeField] Transform LeftWheelView;						//Left wheel view, to rotate the transform
 	[SerializeField] Transform RightWheelView;						//Right wheel view, to rotate the transform
 	[SerializeField] float AngleMiltiplier = 45;					//Rotation multiplier
 
-	[SerializeField] Vector3 LeftWheelPosition;		//For getting position from LeftWheelCollider
-	[SerializeField] Vector3 RightWheelPosition;		//For getting position from RightWheelCollider
-	Transform TransformHelper;		//For save transform at the start
-	[SerializeField] float Distance;					//Distance between wheels, for the angle calculation formula
+	[SerializeField] Vector3 LeftWorldPosition;						//For getting position from LeftWheelCollider
+	[SerializeField] Vector3 RightWorldPosition;					//For getting position from RightWheelCollider
+	[SerializeField] Vector3 LeftWheelPosition;						//For getting position from LeftWheelCollider
+	[SerializeField] Vector3 RightWheelPosition;					//For getting position from RightWheelCollider
+	Transform TransformHelper;										//For save transform at the start
+	[SerializeField] float Distance;								//Distance between wheels, for the angle calculation formula
 
 	private void Awake() {
 		//Saving transform on start
@@ -32,14 +34,17 @@ public class Axle : MonoBehaviour {
 
 		// LeftWheelCollider.GetWorldPose(out LeftWheelPosition, out rot);
 		// RightWheelCollider.GetWorldPose(out RightWheelPosition, out rot);
-		LeftWheelPosition = LeftWheelCollider.WheelPosition;
-		RightWheelPosition = RightWheelCollider.WheelPosition;
+		LeftWorldPosition = LeftWheelPosition = LeftWheelController.position;
+		RightWorldPosition = RightWheelPosition = RightWheelController.position;
 		// rot = LeftWheelCollider.WheelRotation;
 
 		LeftWheelPosition = TransformHelper.transform.InverseTransformPoint(LeftWheelPosition);
 		RightWheelPosition = TransformHelper.transform.InverseTransformPoint(RightWheelPosition);
+		// LeftWheelPosition = TransformHelper.transform.InverseTransformPoint(LeftWheelPosition);
+		// RightWheelPosition = TransformHelper.transform.InverseTransformPoint(RightWheelPosition);
 
 		Distance = RightWheelPosition.x - LeftWheelPosition.x;
+		// Distance = Vector3.Distance(LeftWheelPosition, RightWheelPosition);
 
 		//Calculate axle pos, position is considered the midpoint between the wheels
 		Vector3 newAxlePos = TransformHelper.localPosition;
@@ -54,7 +59,68 @@ public class Axle : MonoBehaviour {
 		transform.localRotation = rot;
 
 		//Wheels rotation assignment, from WheelColliders
-		LeftWheelView.localRotation *= Quaternion.Euler(LeftWheelCollider.RPM * 6 * Time.deltaTime, 0f, 0f);
-		RightWheelView.localRotation *= Quaternion.Euler(RightWheelCollider.RPM * 6 * Time.deltaTime, 0f, 0f);
+		LeftWheelView.localRotation = Quaternion.Euler(LeftWheelController.rotation.x, 0f, 0f);
+		RightWheelView.localRotation = Quaternion.Euler(RightWheelController.rotation.x, 0f, 0f);
 	}
 }
+*/
+
+	using System.Collections;
+	using System.Collections.Generic;
+	using UnityEngine;
+	using NWH.Common.Vehicles;
+
+	/// <summary>
+	/// This script is needed to change the position and rotation the axle
+	/// </summary>
+	public class Axle : MonoBehaviour {
+
+		[SerializeField] WheelUAPI LeftWheelCollider;					//Left WheelCollider ref
+		[SerializeField] WheelUAPI RightWheelCollider;					//Right WheelCollider ref
+		[SerializeField] Transform LeftWheelView;						//Left wheel view, to rotate the transform
+		[SerializeField] Transform RightWheelView;						//Right wheel view, to rotate the transform
+		[SerializeField] float AngleMiltiplier = 45;					//Rotation multiplier
+
+		[SerializeField] Vector3 LeftWheelPosition;						//For getting position from LeftWheelCollider
+		[SerializeField] Vector3 InversedLeftWheelPosition;
+		[SerializeField] Vector3 RightWheelPosition;					//For getting position from RightWheelCollider
+		[SerializeField] Vector3 InversedRightWheelPosition;
+		Transform TransformHelper;										//For save transform at the start
+		[SerializeField] float Distance;								//Distance between wheels, for the angle calculation formula
+
+		private void Awake() {
+			//Saving transform on start
+			TransformHelper = new GameObject("AxleTransformHelper").transform;
+			TransformHelper.SetParent(transform.parent);
+			TransformHelper.position = transform.position;
+		}
+
+		private void FixedUpdate () {
+			//Get wheels position
+			Quaternion rot;
+
+			LeftWheelPosition = LeftWheelCollider.WheelPosition;
+			RightWheelPosition = RightWheelCollider.WheelPosition;
+
+			LeftWheelPosition = TransformHelper.transform.InverseTransformPoint(LeftWheelPosition);
+			RightWheelPosition = TransformHelper.transform.InverseTransformPoint(RightWheelPosition);
+
+			Distance = RightWheelPosition.x - LeftWheelPosition.x;
+
+			//Calculate axle pos, position is considered the midpoint between the wheels
+			Vector3 newAxlePos = TransformHelper.localPosition;
+			newAxlePos.y += (LeftWheelPosition.y + RightWheelPosition.y) * 0.5f;
+			transform.localPosition = newAxlePos;
+
+			//Calculate axle rotation, the angle is calculated from the height difference of the wheels in local space
+			float angle = (LeftWheelPosition.y > RightWheelPosition.y)? -AngleMiltiplier : AngleMiltiplier;
+			angle = Mathf.Abs(LeftWheelPosition.y - RightWheelPosition.y) / Distance * angle;
+			rot = Quaternion.AngleAxis(angle, Vector3.forward);
+
+			transform.localRotation = rot;
+
+			//Wheels rotation assignment, from WheelColliders
+			LeftWheelView.localRotation *= Quaternion.Euler(LeftWheelCollider.RPM * 6 * Time.deltaTime, 0f, 0f);
+			RightWheelView.localRotation *= Quaternion.Euler(RightWheelCollider.RPM * 6 * Time.deltaTime, 0f, 0f);
+		}
+	}
--- Assets/Scripts/Vehicle/TiresFriction.cs
+++ Assets/Scripts/Vehicle/TiresFriction.cs
@@ -9,6 +9,9 @@ public class TiresFriction : MonoBehaviour {
     private VehicleType _vehicleType;
 
     [SerializeField]
+    private LayerMask _raycastLayerMask;
+
+    [SerializeField]
     private float _baseFriction = 1.0f;
     [SerializeField]
     private float _tireIntegrity;
@@ -19,23 +22,15 @@ public class TiresFriction : MonoBehaviour {
     [SerializeField]
     private float _totalGroundFriction;
     [SerializeField]
-    private WheelUAPI[] _wheelsColliders;
-    [SerializeField]
+    private WheelUAPI[] _wheelsControllers;
+    [SerializeField] 
     private SurfaceType[] _surfaceType;
     [SerializeField]
+    private float[] _springLength;
+    [SerializeField]
     private float[] _wheelSlip;
-
-    // [Header("Передняя ось авто")]
-    // [SerializeField]
-    // private FrictionSettings frontForwardFriction;
-    // [SerializeField]
-    // private FrictionSettings frontSidewaysFriction;
-
-    // [Header("Задняя ось авто")]
-    // [SerializeField]
-    // private FrictionSettings rearForwardFriction;
-    // [SerializeField]
-    // private FrictionSettings rearSidewaysFriction;
+    [SerializeField]
+    private float[] _rayDistance;
 
     private float[] _originExtremumValue;
     Dictionary<string, SurfaceType> materialSurfaceMap = new Dictionary<string, SurfaceType>
@@ -63,160 +58,63 @@ public class TiresFriction : MonoBehaviour {
     public float massMultiplier => this._massMultiplier;
     public float totalGroundFriction => this._totalGroundFriction;
     public float[] typeMultiplier => this._typeMultiplier;
-    public WheelUAPI[] wheelsColliders => this._wheelsColliders;
+    public WheelUAPI[] wheelsControllers => this._wheelsControllers;
     public SurfaceType[] surfaceType => this._surfaceType;
     public float[] wheelSlip => this._wheelSlip;
 
     private void Start() {
         this.VehicleManager = GetComponent<VehicleManager>();
-        // InitializeFrictionFactors();
+        InitializeValues();
     }
 
     private void FixedUpdate() {
-        // DetermineSurfaceType();
-        // ApplyInclineAngle();
-        // ApplyFriction();
+        DetermineSurfaceType();
+        // ApplyFrictionModel();
     }
 
-/* Старый вариант определения сцепления
     private void DetermineSurfaceType() {
-        for (int i = 0; i < _wheelsColliders.Length; i++) {
-            WheelCollider wheelCollider = _wheelsColliders[i];
+        for (int wheelIndex = 0; wheelIndex < this._wheelsControllers.Length; wheelIndex++) {
 
-            WheelHit hit;
+            RaycastHit hit;
+            Vector3 raycastOrigin = this._wheelsControllers[wheelIndex].transform.position;
 
-            if(wheelCollider.GetGroundHit(out hit)) {
+            this._springLength[wheelIndex] = this._wheelsControllers[wheelIndex].SpringLength;
+            this._rayDistance[wheelIndex] = this._wheelsControllers[wheelIndex].SpringLength + this._wheelsControllers[wheelIndex].Radius + 0.1f;
+
+            Debug.DrawRay(raycastOrigin, -this._wheelsControllers[wheelIndex].transform.up * this._rayDistance[wheelIndex], Color.red, 0.1f);
+            if(Physics.Raycast(raycastOrigin, -this._wheelsControllers[wheelIndex].transform.up, out hit, this._rayDistance[wheelIndex])) {
                 PhysicMaterial currentMaterial = hit.collider.sharedMaterial;
 
-                // Присваиваем typeMultiplier в зависимости от типа поверхности
-                if (currentMaterial != null) {
-                    string materialName = currentMaterial.name.ToLower();
-
-                    if(materialName.Contains("asphalt")) {
-                        _typeMultiplier[i] = (materialName.Contains("wet")) ? 0.65f : 0.85f;
-                        _surfaceType[i] = (materialName.Contains("wet")) ? SurfaceType.AsphaltWet : SurfaceType.Asphalt;
-                    } else if(materialName.Contains("concrete")) {
-                        _typeMultiplier[i] = (materialName.Contains("wet")) ? 0.7f : 0.8f;
-                        _surfaceType[i] = (materialName.Contains("wet")) ? SurfaceType.ConcreteWet : SurfaceType.Concrete;
-                    } else if(materialName.Contains("sand")) {
-                        _typeMultiplier[i] = (materialName.Contains("wet")) ? 0.55f : 0.45f;
-                        _surfaceType[i] = (materialName.Contains("wet")) ? SurfaceType.SandWet : SurfaceType.Sand;
-                    } else if(materialName.Contains("dirt")) {
-                        _typeMultiplier[i] = (materialName.Contains("wet")) ? 0.45f : 0.55f;
-                        _surfaceType[i] = (materialName.Contains("wet")) ? SurfaceType.DirtWet : SurfaceType.Dirt;
-                    } else if(materialName.Contains("snow")) {
-                        _typeMultiplier[i] = (materialName.Contains("icy")) ? 0.2f : 0.3f;
-                        _surfaceType[i] = (materialName.Contains("icy")) ? SurfaceType.SnowIcy : SurfaceType.Snow;
-                    } else if(materialName.Contains("ice")) {
-                        _typeMultiplier[i] = 0.18f;
-                        _surfaceType[i] = SurfaceType.Ice;
-                    }
-                }
+                this._surfaceType[wheelIndex] = DetermineSurfaceType(currentMaterial);
             }
         }
     }
-*/
 
-    // private void DetermineSurfaceType() {
-    //     for (int i = 0; i < _wheelsColliders.Length; i++) {
-    //         WheelUAPI wheelCollider = _wheelsColliders[i];
+    private SurfaceType DetermineSurfaceType(PhysicMaterial material) {
+        string materialName = material.name.ToLower();
 
-    //         WheelHit hit;
+        SurfaceType surfaceType = (materialSurfaceMap.TryGetValue(materialName, out SurfaceType baseSurfaceType)) ? baseSurfaceType : (materialName.Contains("wet") && wetMaterialSurfaceMap.TryGetValue(materialName, out SurfaceType wetSurfaceType)) ? wetSurfaceType : SurfaceType.Undefined;
 
-    //         if(wheelCollider.GetGroundHit(out hit)) {
-    //             PhysicMaterial currentMaterial = hit.collider.sharedMaterial;
+        return surfaceType;
+    }
 
-    //             // Получаем сцепление из коллайдера на земле
-    //             // this._typeMultiplier[i] = currentMaterial.dynamicFriction;
-    //             this._surfaceType[i] = DetermineSurfaceType(currentMaterial);
-    //         }
-    //     }
-    // }
-
-    // private SurfaceType DetermineSurfaceType(PhysicMaterial material) {
-    //     string materialName = material.name.ToLower();
-
-    //     SurfaceType surfaceType = (materialSurfaceMap.TryGetValue(materialName, out SurfaceType baseSurfaceType)) ? baseSurfaceType : (materialName.Contains("wet") && wetMaterialSurfaceMap.TryGetValue(materialName, out SurfaceType wetSurfaceType)) ? wetSurfaceType : SurfaceType.Undefined;
-
-    //     return surfaceType;
-    // }
-
-    // private void ApplyInclineAngle() {
-    //     for (int i = 0; i < _wheelsColliders.Length; i++) {
-    //         WheelCollider wheelCollider = _wheelsColliders[i];
-
-    //         float wheelInclineAngle = Vector3.Angle(wheelCollider.transform.up, Vector3.up);
-    //         float inclineEffect = Mathf.Clamp01(1 - (wheelInclineAngle / 90f));
-
-    //         // Применяем эффект наклона к фактору сцепления
-    //         _typeMultiplier[i] *= inclineEffect;
-    //     }
-    // }
-
-    // private void ApplyFriction() {
-    //     WheelHit hit;
-    //     float frictionSum = 0f;
-    //     for(int i = 0; i < _wheelsColliders.Length; i++) {
-    //         WheelCollider wheelCollider = _wheelsColliders[i];
-
-    //         WheelFrictionCurve forwardFriction = wheelCollider.forwardFriction;
-    //         WheelFrictionCurve sidewaysFriction = wheelCollider.sidewaysFriction;
-
-    //         forwardFriction.stiffness = this._typeMultiplier[i] * (0.25f * Mathf.Sqrt(this._tireIntegrity) + 0.75f);
-    //         sidewaysFriction.stiffness = this._typeMultiplier[i] * (0.25f * Mathf.Sqrt(this._tireIntegrity) + 0.75f);
-    //         sidewaysFriction.extremumValue = ((i > 1) && this.VehicleManager.VehicleInputHandler.handbrake && (this.VehicleManager.VehicleInputHandler.horizontal > 0.5f || this.VehicleManager.VehicleInputHandler.horizontal < -0.5f)) ? 0.2f : this._originExtremumValue[i];
-
-    //         wheelCollider.forwardFriction = forwardFriction;
-    //         wheelCollider.sidewaysFriction = sidewaysFriction;
-    //         if(this._wheelsColliders[i].GetGroundHit(out hit)) {
-    //             this._wheelSlip[i] = Mathf.Abs(hit.forwardSlip) + Mathf.Abs(hit.sidewaysSlip);
-    //         }
-    //         frictionSum += this._typeMultiplier[i];
-    //     }
-    //     this._totalGroundFriction = frictionSum / this._wheelsColliders.Length;
-    // }
-
-    // private void InitializeFrictionFactors() {
-    //     VehicleData vehicleData = this.VehicleManager.vehicleData;
-
-    //     this._vehicleType = vehicleData.type;
-    //     this._baseFriction = (this._vehicleType == VehicleType.TrophyTruck) ? 0.775f : (this._vehicleType == VehicleType.Truck) ? 0.8f : (this._vehicleType == VehicleType.Prerunner) ? 0.85f : (this._vehicleType == VehicleType.Buggy) ? 0.9f : 0.85f;
-        
-    //     // Учет массы для настройки сцепления
-    //     this._massMultiplier = 1 - vehicleData.normalizedMass;
-    //     this._tireIntegrity = vehicleData.tireIntegrity;
-        
-    //     Transform carCollidersTransform = gameObject.transform.Find("carColliders");
-        
-    //     if(carCollidersTransform != null) {
-    //         int wheelCount = carCollidersTransform.childCount;
-    //         this._wheelsColliders = new WheelCollider[wheelCount];
-    //         this._typeMultiplier = new float[wheelCount];
-    //         this._surfaceType = new SurfaceType[wheelCount];
-    //         this._originExtremumValue = new float[wheelCount];
-    //         this._wheelSlip = new float[wheelCount];
+    private void InitializeValues() {
+        Transform carCollidersTransform = gameObject.transform.Find("carColliders");
+        if(carCollidersTransform != null) {
+            int wheelCount = carCollidersTransform.childCount;
+            this._wheelsControllers = new WheelUAPI[wheelCount];
+            // this._typeMultiplier = new float[wheelCount];
+            this._surfaceType = new SurfaceType[wheelCount];
+            this._springLength = new float[wheelCount];
+            this._rayDistance = new float[wheelCount];
+            // this._originExtremumValue = new float[wheelCount];
+            // this._wheelSlip = new float[wheelCount];
             
-    //         for(int q = 0; q < wheelCount; q++) {
-    //             this._wheelsColliders[q] = carCollidersTransform.GetChild(q).GetComponent<WheelCollider>();
-
-    //             this._wheelsColliders[q].forwardFriction = CustomFrictionCurve(q < 2 ? frontForwardFriction : rearForwardFriction);
-    //             this._wheelsColliders[q].sidewaysFriction = CustomFrictionCurve(q < 2 ? frontSidewaysFriction : rearSidewaysFriction);
-
-    //             this._originExtremumValue[q] = this._wheelsColliders[q].sidewaysFriction.extremumValue;
-    //         }
-    //     }
-    // }
-
-    // private WheelFrictionCurve CustomFrictionCurve(FrictionSettings frictionSettings) {
-    //     WheelFrictionCurve frictionCurve = new WheelFrictionCurve();
-
-    //     frictionCurve.extremumSlip = frictionSettings.extremumSlip[1] - this._massMultiplier * (frictionSettings.extremumSlip[1] - frictionSettings.extremumSlip[0]);
-    //     frictionCurve.extremumValue = frictionSettings.extremumValue[1] - this._massMultiplier * (frictionSettings.extremumValue[1] - frictionSettings.extremumValue[0]);
-    //     frictionCurve.asymptoteSlip = frictionSettings.asymptoteSlip[1] - this._massMultiplier * (frictionSettings.asymptoteSlip[1] - frictionSettings.asymptoteSlip[0]);
-    //     frictionCurve.asymptoteValue = frictionSettings.asymptoteValue[1] - this._massMultiplier * (frictionSettings.asymptoteValue[1] - frictionSettings.asymptoteValue[0]);
-
-    //     return frictionCurve;
-    // }
+            for(int q = 0; q < wheelCount; q++) {
+                this._wheelsControllers[q] = carCollidersTransform.GetChild(q).GetComponent<WheelUAPI>();
+            }
+        }
+    }
 }
 
 [System.Serializable]
@@ -235,11 +133,3 @@ public enum SurfaceType {
     Undefined,
     IFriction
 }
-
-// [System.Serializable]
-// public class FrictionSettings {
-//     public float[] extremumSlip = new float[2];
-//     public float[] extremumValue = new float[2];
-//     public float[] asymptoteSlip = new float[2];
-//     public float[] asymptoteValue = new float[2];
-// }
